1ea4229364cc6788cb5c3d92a41beda4e86e3749
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2024-12-31 01:03:35 -0300

    Acessibilidade da página foi aprimorada

--- scripts/components/modals.js
+++ scripts/components/modals.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container')
 const btnSearch = document.querySelector('.btn-search')
 let modalContent;
+let btnClose;
 
 export const modalError = (messageError) => {
     const modal = document.createElement('div')
@@ -12,7 +13,9 @@ export const modalError = (messageError) => {
                 <ion-icon name="alert-circle"></ion-icon>
                 <h1 class="modal-title">Houve um erro...</h1>
             </div>
-            <ion-icon class="modal-close" name="close-outline"></ion-icon>
+            <button class="modal-close">
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+            </button>
         </div>
         <h3 class="modal-info">${messageError}</h3>
     </div> `
@@ -20,10 +23,22 @@ export const modalError = (messageError) => {
     container.appendChild(modal)
     modal.classList.add('active')
     document.querySelector('.modal').classList.add('active')
+    btnClose = document.querySelector('.modal-close')
+    btnClose.focus()
+    // Desabilita a tecla TAB para não interagir com outros botões
+    document.addEventListener('keydown', disableTab)
     btnSearch.disabled = false
     modalContent = modal
 }
 
 export const closeModalError = () => {
     container.removeChild(modalContent)
+    document.removeEventListener('keydown', disableTab)
+}
+
+const disableTab = (e) => {
+    if(e.key === 'Tab') {
+        e.preventDefault();
+        btnClose.focus()
+    }
 }
