30ef31d4bc57f9149f9ace18dbf44c16dc040aaa
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-01-01 23:05:18 -0300

    Melhorias significativas no código, bugs corrijido, acessibilidade melhorada.

--- scripts/components/cnpjList.js
+++ scripts/components/cnpjList.js
@@ -1,65 +1,65 @@
-const menuList = document.querySelector('.menu-list')
+const menuList = document.querySelector(".menu-list");
 const menuListContent = document.querySelector(".menu-list-content");
 const arrowIndicator = document.querySelector(".arrow-indicator");
-const btnList = document.querySelector('.btn-list')
-const cnpj = document.querySelectorAll('.cnpj')
-const field = document.querySelector('.label')
-const input = document.querySelector('.field')
-const pencilIcon = document.querySelector('.pencil-icon')
+const btnList = document.querySelector(".btn-list");
+const cnpj = document.querySelectorAll(".cnpj");
+const field = document.querySelector(".label");
+const input = document.querySelector(".field");
+const pencilIcon = document.querySelector(".pencil-icon");
 
 export const openList = (e) => {
   menuListContent.classList.toggle("active");
   arrowIndicator.classList.toggle("active");
-  btnList.classList.toggle("active")
+  btnList.classList.toggle("active");
 };
 
 export const closeOutsideList = (e) => {
   if (!menuList.contains(e.target) && !field.contains(e.target)) {
-    removeMenu()
+    removeMenu();
   }
 };
 
 export const closePressList = (e) => {
-  if (e.key === 'Escape') {
-    removeMenu()
+  if (e.key === "Escape") {
+    removeMenu();
   }
 };
 
 const removeMenu = () => {
   menuListContent.classList.remove("active");
   arrowIndicator.classList.remove("active");
-  btnList.classList.remove("active")
-}
+  btnList.classList.remove("active");
+};
 
 export const addMenu = () => {
   menuListContent.classList.add("active");
   arrowIndicator.classList.add("active");
-  btnList.classList.add("active")
-}
+  btnList.classList.add("active");
+};
 
 export const copyToClipboard = (e) => {
-  cnpj.forEach(item => {
+  cnpj.forEach((item) => {
     if (e.target.previousElementSibling === item) {
-      const text = item.innerText
-      navigator.clipboard.writeText(text)
-        .then(() => {
-          let copyText = e.target.lastChild.previousElementSibling
-          copyText.innerText = 'Copiado'
-          e.target.style.backgroundColor = 'green';
-          input.focus()
+      const text = item.innerText;
+
+      if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(text).then(() => {
+          e.target.innerHTML = `
+              <ion-icon class="pencil-icon" name="checkmark-done-outline"></ion-icon>
+              <span class="copy-text">Copiado</span>
+          `;
+          e.target.style.backgroundColor = "green";
+          input.focus();
 
           setTimeout(() => {
-            copyText.innerText = 'Copiar'
-            e.target.style.backgroundColor = '#5d5fef'
-          }, 2000)
-        })
-      if (navigator.clipboard && navigator.clipboard.writeText) {
-
+            e.target.innerHTML = `
+                <ion-icon class="pencil-icon" name="pencil-sharp"></ion-icon>
+                <span class="copy-text">Copiar</span>
+            `;
+            e.target.style.backgroundColor = "#5d5fef";
+          }, 2000);
+        });
       }
-
     }
-  })
-}
-
-
-
+  });
+};
