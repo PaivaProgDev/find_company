4c426aa87f25d0b4fae97129bc15a6feec0eb816
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-01-03 17:20:14 -0300

    Função implementada para copiar os cnpj para a área de transferência nos mobiles

--- scripts/components/cnpjList.js
+++ scripts/components/cnpjList.js
@@ -33,25 +33,37 @@ const removeMenu = () => {
 export const copyToClipboard = (e) => {
   cnpj.forEach((item) => {
     if (e.target.previousElementSibling === item) {
-      const text = item.innerText;
+      const text = item.innerHTML;
 
       if (navigator.clipboard && navigator.clipboard.writeText) {
-        navigator.clipboard.writeText(text).then(() => {
-          e.target.innerHTML = `
+        navigator.clipboard
+          .writeText(text)
+          .then(() => {
+            e.target.innerHTML = `
               <ion-icon class="pencil-icon" name="checkmark-done-outline"></ion-icon>
               <span class="copy-text">Copiado</span>
           `;
-          e.target.style.backgroundColor = "green";
-          input.focus();
+            e.target.style.backgroundColor = "green";
+            input.focus();
 
-          setTimeout(() => {
-            e.target.innerHTML = `
+            setTimeout(() => {
+              e.target.innerHTML = `
                 <ion-icon class="pencil-icon" name="pencil-sharp"></ion-icon>
                 <span class="copy-text">Copiar</span>
             `;
-            e.target.style.backgroundColor = "#5d5fef";
-          }, 2000);
-        });
+              e.target.style.backgroundColor = "#5d5fef";
+            }, 2000);
+          })
+          .catch((err) => {
+            alert("Erro ao copiar o texto");
+          });
+      } else {
+        try {
+          item.select();
+          document.execCommand("copy");
+        } catch (err) {
+          alert("Erro ao copiar o texto");
+        }
       }
     }
   });
